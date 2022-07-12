d3a9aa19bcc6180981b3a6d3620048f334e914d7
Samet Yıldırım <smtyldrm32@gmail.com>
2022-07-12 21:06:08 +0300

    Create kosullu_eleman_islemleri.py

---
+++ veri_manipulasyonu/kosullu_eleman_islemleri.py
@@ -0,0 +1,20 @@
+# Kosullu Eleman Islemleri
+
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+print(a < 3)
+print(a[a < 3])
+print(a[a > 3])
+print(a[a <= 3])
+print(a[a >= 3])
+print(a[a == 3])
+print(a[a != 3])
+
+print("----------MATEMATIK----------")
+
+print(a)
+print(a*5)
+print(a**2)
+print(a+25)
